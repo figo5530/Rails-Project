79711080c273117427a3f695397254c3151c1f7b
figo5530 <figooooo0221@gmail.com>
2021-03-30 00:53:23 -0400

    adds logout and signin route, creates #destroy in SessionsController to log out, adds logout link in show.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'users#home'
   resources :users
   get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
