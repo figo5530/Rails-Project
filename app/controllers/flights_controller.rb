45168bc28190ffbcac06c5cbf38d28b97d886fdb
figo5530 <figooooo0221@gmail.com>
2021-03-30 17:05:42 -0400

    changes index.erb and adds #show show.erb in flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,8 @@ class FlightsController < ApplicationController
     def index
         @flights = Flight.all
     end
+
+    def show
+        @flight = Flight.find_by(id: params[:id])
+    end
 end
