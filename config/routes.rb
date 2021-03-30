5a4325f6e9d0cc1ea6e6e3b0fc7b72e0b18d36d3
figo5530 <figooooo0221@gmail.com>
2021-03-30 00:43:00 -0400

    adds signin routes, create SessionsController and #new in it, adds three links in home.erb, implements sessions/new.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#home'
   resources :users
+  get '/signin', to: 'sessions#new'
 end
